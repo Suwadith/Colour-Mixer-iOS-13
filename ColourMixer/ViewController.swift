6afc1af244b544677d529587080e3dfbd176e8a8
Suwadith <suwadith.srithar@gmail.com>
2020-02-05 01:22:16 +0530

    update

--- ColourMixer/ViewController.swift
+++ ColourMixer/ViewController.swift
@@ -15,6 +15,35 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBOutlet weak var redLevel: UILabel!
+    
+    @IBOutlet weak var greenLevel: UILabel!
+    
+    @IBOutlet weak var blueLevel: UILabel!
+    
+    @IBOutlet weak var colorPalette: UIView!
+    
+    
+    @IBAction func redSlider(_ sender: UISlider) {
+        redLevel.text = String(Int(sender.value))
+//        colorPalette.backgroundColor = UIColor(red: CGFloat(sender.value), green: 0.0, blue: 0.0, alpha: 1.0)
+    }
+    
+    @IBAction func greenSlider(_ sender: UISlider) {
+        greenLevel.text = String(Int(sender.value))
+//        colorPalette.backgroundColor = UIColor.green.withAlphaComponent(CGFloat(sender.value))
+    }
+    
+    @IBAction func blueSlider(_ sender: UISlider) {
+        blueLevel.text = String(Int(sender.value))
+//        colorPalette.backgroundColor = UIColor.blue.withAlphaComponent(CGFloat(sender.value))
+    }
+    
+    
+    
+    
+    
 }
+
 
